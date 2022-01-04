2fdfd35d0cc2d65c35494558963b1a0407d2be1e
Jaydeep Suthar <technicaljade@gmail.com>
2022-01-04 17:19:58 +0530

    Weird changes
    some weird changes in code is happening
    
    Signed-off-by: Jaydeep Suthar <technicaljade@gmail.com>

--- src/app.js
+++ src/app.js
@@ -5,16 +5,31 @@ const helmet = require('helmet');
 const logger = require('morgan');
 const cors = require('cors');
 const { connect } = require('mongoose');
+const nunjucks = require('nunjucks');
+const path = require('path');
 
 // * INIT
 const app = express();
 const port = process.env.PORT || 8080;
 
-// * DB
-// connect(process.env.MONGO_URI, console.log(`DB CONNECTED`));
+// * NUNJUCKS SETTING
+app.set('views', path.join(`${__dirname}/views`));
+app.set('view engine', 'html');
+
+nunjucks.configure('src/views', {
+    autoescape: true,
+    express: app
+});
+
+
+// * VIEW
+app.use('public', express.static(path.resolve(`${__dirname}/public`)));
+app.use(express.urlencoded({ extended: false }));
+
 
 // * MIDDLEWARES
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use(helmet());
 app.use(cors());
@@ -25,7 +40,8 @@ app.use('/api/course', require('./routes/course'));
 
 // * ROUTES
 app.get('/', (req, res) => {
-    res.send({ msg: 'hello programmeres' });
+//    res.render('pages/home.html');
+	res.json({ msg: "Hello from backend" });
 });
 
 // *! 404 ERROR PAGE THIS MUST BE A THE END AFTER ALL ROUTES
---
+++ src/routes/user/index.js
@@ -0,0 +1,168 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
+// * VALIDATION
+const { registerUserValidation, loginUserValidation, generateToken } = require('../../helper/validation');
+const { isLoggedIn } = require('../../middlewares/auth');
+
+// * Prisma
+const prisma = require('../../helper/prisma');
+
+// * LOGIN ROUTE
+
+router.get('/login', (req, res) => {
+    res.render('pages/login.html');
+});
+
+router.post('/login', async (req, res) => {
+
+    console.table(req.body);
+    const { email, password } = req.body;
+
+    const { error, value } = loginUserValidation({ email, password });
+    if (error) return res.status(400).json({ 'error': error.details[0].message });
+
+    // Check email and password is valid
+    const user = await prisma.users.findUnique({
+        where: {
+            email: email
+        },
+
+        select: {
+            id: true,
+            email: true,
+            password: true
+        }
+    });
+    if (!user) return res.status(400).json({ "msg": "No Account Found On This Email Please Create Account First" });
+
+    // check password hash is valid
+    const isValidPass = await bcrypt.compare(password, user.password);
+    if (!isValidPass) return res.status(400).json({ "msg": "Email and Password Doesn't match" });
+
+    // const token = jwt.sign
+    // ! solve the role error
+    const token = generateToken(user.id, user.role);
+
+    res.header("authorization", token);
+    // res.json({
+    //     "msg": "you are logged in",
+    //     "id": user.id,
+    //     "role": user.role
+    // });
+    res.send('Data received:\n' + JSON.stringify(req.body));
+    // res.end();
+});
+
+// * SIGNUP ROUTE / ADD USER
+router.post('/signup', async (req, res) => {
+
+    const { name, email, contact, password } = req.body;
+
+    // * check data is valid
+    const { error, value } = registerUserValidation({ name, email, contact, password });
+    if (error) return res.status(400).json({ 'error': error.details[0].message });
+
+    // check user email already exists
+    const userExists = await prisma.users.findUnique({
+        where: {
+            email: email
+        }
+    });
+    if (userExists) return res.status(400).json({ "msg": "Email is already register" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // * saving it in db
+    const user = {
+        name: name,
+        email: email,
+        contact: contact,
+        password: hashedPassword,
+        qualification: 'higher_secondary'
+    };
+
+
+    try {
+        const newUser = await prisma.users.create({ data: user, select: { id: true, email: true } });
+        res.json({
+            msg: "User Created",
+            "ID": newUser.id,
+            "Email": newUser.email
+        });
+    } catch (err) {
+        res.status(400).json(`Error Occur`);
+    }
+
+    res.end();
+});
+
+// * EDIT USER
+router.patch('/update/:user_id', async (req, res) => {
+
+    const id = req.params.user_id;
+    const { name, contact, password } = req.body;
+
+    // Check email and password is valid
+    const user = await prisma.users.findUnique({
+        where: {
+            id: id
+        }
+    });
+    if (!user) return res.status(400).json({ "msg": "No Account Found On This Email Please Create Account First" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // * saving it in db
+    const userData = {
+        name: name,
+        contact: contact,
+        password: hashedPassword,
+        qualification: 'higher_secondary'
+    };
+
+    try {
+        const updatedUser = await prisma.users.update({
+            where: { id: id },
+            data: userData
+        });
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json(`Error Occur`);
+        throw err;
+    }
+
+    res.end();
+});
+
+// * DELETE USER
+router.delete('/delete/:user_id', async (req, res) => {
+
+    const id = req.params.user_id;
+
+    // Check email and password is valid
+    const user = await prisma.users.findUnique({
+        where: {
+            id: id
+        }
+    });
+    if (!user) return res.status(400).json({ "msg": "No Account Found On This Email Please Create Account First" });
+
+    try {
+        const deleteUser = await prisma.users.delete({
+            where: {
+                id: id
+            }
+        });
+        res.json({
+            msg: `User Succesfully Deleted`
+        });
+    } catch (err) {
+        res.status(400).json(`Error Occur ${err}`);
+        // throw err;
+    }
+
+    res.end();
+});
+
+module.exports = router;
