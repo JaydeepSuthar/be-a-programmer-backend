6680c8b308e4f92351f6b1ac65fe9761403fa882
Jaydeep Suthar <technicaljade@gmail.com>
2022-03-29 20:27:33 +0530

    New Chapters automatically have last + 1 SrNo

--- src/routes/course/chapter/index.js
+++ src/routes/course/chapter/index.js
@@ -1,57 +1,66 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // * VALIDATION
-const { isLoggedIn, isAdmin } = require('../../../middlewares/auth');
+const { isLoggedIn, isAdmin } = require("../../../middlewares/auth");
 
 // * Prisma
-const prisma = require('../../../helper/prisma');
+const prisma = require("../../../helper/prisma");
 
 /**
  * @desc Get all Chapters of Particular Course
  */
-router.get('/:course_id', async (req, res) => {
-
+router.get("/:course_id", async (req, res) => {
 	const { course_id } = req.params;
 
 	try {
 		const allChapters = await prisma.chapters.findMany({
 			where: {
-				course_detailsId: course_id
+				course_detailsId: course_id,
 			},
 			include: {
 				_count: {
 					select: {
 						videos: true,
-						assignments: true
-					}
-				}
-
-			}
+						assignments: true,
+					},
+				},
+			},
 		});
 		console.log(JSON.stringify(allChapters, null, 2));
 		// console.table(allChapters);
-		return res.status(200).json({ is_success: true, msg: `Chapters found`, data: allChapters });
+		return res.status(200).json({
+			is_success: true,
+			msg: `Chapters found`,
+			data: allChapters,
+		});
 	} catch (err) {
 		console.log(err.message);
-		return res.status(404).json({ is_success: false, msg: `Chapters not found`, error: err.message });
+		return res.status(404).json({
+			is_success: false,
+			msg: `Chapters not found`,
+			error: err.message,
+		});
 	}
 });
-
 
 /**
  * @desc Get Single Chapter
  */
 // ? i don't think this is neccessary now
-router.get('/:chapter_id', async (req, res) => {
-
+router.get("/:chapter_id", async (req, res) => {
 	const { chapter_id } = req.params;
 
 	// * check chapter exists or not
-	const chapterExists = await prisma.chapters.count({ where: { id: chapter_id } });
-	if (!chapterExists) return res.status(409).json({ error: `Chapter Doesn't Exists` });
+	const chapterExists = await prisma.chapters.count({
+		where: { id: chapter_id },
+	});
+	if (!chapterExists)
+		return res.status(409).json({ error: `Chapter Doesn't Exists` });
 
 	try {
-		const chapter = await prisma.chapters.findUnique({ where: { id: chapter_id } });
+		const chapter = await prisma.chapters.findUnique({
+			where: { id: chapter_id },
+		});
 		return res.status(200).json({ msg: `Chapter Details`, data: chapter });
 	} catch (err) {
 		console.log(err.message);
@@ -63,24 +72,49 @@ router.get('/:chapter_id', async (req, res) => {
  * @desc Add Chapter
  */
 
-router.post('/add', isLoggedIn, isAdmin, async (req, res) => {
-
-	const { srno, name, is_visible, course_id } = req.body || '';
+router.post("/add", isLoggedIn, isAdmin, async (req, res) => {
+	const { name, is_visible, course_id } = req.body || "";
 
 	// * check course exists or not
-	const courseExists = await prisma.course_details.count({ where: { id: course_id } });
-	if (!courseExists) return res.status(409).json({ error: `Course Doesn't Exists` });
+	const courseExists = await prisma.course_details.count({
+		where: { id: course_id },
+	});
+	if (!courseExists)
+		return res.status(409).json({ error: `Course Doesn't Exists` });
 
 	// * check chapter with same name exists in course
 	// const chapterWithNameExists = await prisma.chapters.count({ where: { AND: { chapter_name: { contains: chapter_name }, course_detailsId: course_id } } });
 	// if (chapterWithNameExists) return res.status(409).json({ error: `Chapter with Same Name Exists` });
 
+	const courseWithAllChaptersSrno = await prisma.course_details.findUnique({
+		where: {
+			id: course_id,
+		},
+		include: {
+			chapters: {
+				select: {
+					srno: true,
+				},
+				orderBy: {
+					srno: "desc",
+				},
+			},
+		},
+	});
+	let lastSrNumber = courseWithAllChaptersSrno.chapters[0];
+
+	if (lastSrNumber) {
+		lastSrNumber = lastSrNumber.srno + 1;
+	} else {
+		lastSrNumber = 1;
+	}
+
 	// * add chapter
 	const chapter = {
-		srno: parseInt(srno),
+		srno: lastSrNumber,
 		chapter_name: name,
 		is_visible: is_visible,
-		course_detailsId: course_id
+		course_detailsId: course_id,
 	};
 
 	try {
@@ -90,12 +124,14 @@ router.post('/add', isLoggedIn, isAdmin, async (req, res) => {
 				_count: {
 					select: {
 						videos: true,
-						assignments: true
-					}
-				}
-			}
+						assignments: true,
+					},
+				},
+			},
 		});
-		return res.status(200).json({ msg: `new chapter created`, data: newChapter });
+		return res
+			.status(200)
+			.json({ msg: `new chapter created`, data: newChapter });
 	} catch (err) {
 		console.error(err.message);
 		return res.status(409).json({ error: err.message });
@@ -106,14 +142,16 @@ router.post('/add', isLoggedIn, isAdmin, async (req, res) => {
  * @desc Edit Chapter
  */
 
-router.put('/edit/:chapter_id', isLoggedIn, isAdmin, async (req, res) => {
-
+router.put("/edit/:chapter_id", isLoggedIn, isAdmin, async (req, res) => {
 	const { chapter_id } = req.params || 0;
 	const { srno, chapter_name, is_visible } = req.body;
 
 	// * check chapter exists or not
-	const chapterExists = await prisma.chapters.count({ where: { id: chapter_id } });
-	if (!chapterExists) return res.status(409).json({ error: `Chapter Doesn't Exists` });
+	const chapterExists = await prisma.chapters.count({
+		where: { id: chapter_id },
+	});
+	if (!chapterExists)
+		return res.status(409).json({ error: `Chapter Doesn't Exists` });
 
 	// * updating chapter
 	const chapter = {
@@ -123,8 +161,13 @@ router.put('/edit/:chapter_id', isLoggedIn, isAdmin, async (req, res) => {
 	};
 
 	try {
-		const updatedChapter = await prisma.chapters.update({ data: chapter, where: { id: chapter_id } });
-		return res.status(200).json({ msg: `chapter edited successfully`, data: updatedChapter });
+		const updatedChapter = await prisma.chapters.update({
+			data: chapter,
+			where: { id: chapter_id },
+		});
+		return res
+			.status(200)
+			.json({ msg: `chapter edited successfully`, data: updatedChapter });
 	} catch (err) {
 		console.log(err.message);
 		return res.status(409).json({ error: `Something went wrong` });
@@ -135,13 +178,17 @@ router.put('/edit/:chapter_id', isLoggedIn, isAdmin, async (req, res) => {
  * @desc Delete Chapter
  */
 
-router.delete('/delete/:chapter_id', isLoggedIn, isAdmin, async (req, res) => {
+router.delete("/delete/:chapter_id", isLoggedIn, isAdmin, async (req, res) => {
 	const { chapter_id } = req.params || 0;
 
 	// * find and delete chapter
 	try {
-		const deletedChapter = await prisma.chapters.delete({ where: { id: chapter_id } });
-		return res.status(200).json({ msg: `Successfully deleted chapter ${deletedChapter.chapter_name}` });
+		const deletedChapter = await prisma.chapters.delete({
+			where: { id: chapter_id },
+		});
+		return res.status(200).json({
+			msg: `Successfully deleted chapter ${deletedChapter.chapter_name}`,
+		});
 	} catch (err) {
 		return res.status(409).json({ msg: `Error Occur`, error: err.message });
 	}
