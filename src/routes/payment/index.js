064922ec7fb202467883c829dd8c08dd2aa819b8
Jaydeep Suthar <technicaljade@gmail.com>
2022-03-30 20:23:29 +0530

    Images minified

--- src/routes/payment/index.js
+++ src/routes/payment/index.js
@@ -7,26 +7,49 @@ const { isLoggedIn, isAdmin } = require("../../middlewares/auth");
 const prisma = require("../../helper/prisma");
 
 // * Razorpay
-const { generateOrder } = require('../../helper/payment');
+// const { generateOrder } = require('../../helper/payment');
+
+const Razorpay = require('razorpay');
+const { nanoid } = require('nanoid');
+
+
+const razorpay = new Razorpay({
+	key_id: process.env.RAZORPAY_KEY_ID,
+	key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
 
 /**
  * @desc Payment Route
  */
 // TODO: add payment route
 router.post('/create', async (req, res) => {
-	console.log(req.body);
+	// console.log(req.body);
 
 	const course = await prisma.course_details.findUnique({
 		where: {
 			id: req.body.course_id
 		}
 	});
-	console.log(course.price);
+	// console.log(course.price);
 
 	const coursePrice = course.price;
-	const order = await generateOrder(coursePrice);
+	// const order = await generateOrder(coursePrice);
+	const order = await razorpay.orders.create({
+		amount: coursePrice * 100,
+		receipt: `Receipt: ${nanoid()}`,
+		payment_capture: 1
+	});
 
-	return res.status(200).json({ is_success: true, msg: `Order Generated`, data: order });
+	console.log(order);
+
+	const orderData = {
+		id: order.id,
+		amount: order.amount,
+		receipt: order.receipt,
+	};
+
+	return res.status(200).json({ is_success: true, msg: `Order Generated`, data: orderData });
 });
 
 module.exports = router;
